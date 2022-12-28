011e2a1d6c709e4f3eafe350b464207dd066af8b
Juan Jose Mayorga <devjuanjose@gmail.com>
2022-12-28 14:51:32 -0600

    adding static pokemons in the main page

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import Head from 'next/head'
+import { Navbar } from '../ui';
 
 interface Props {
   title?: string;
@@ -16,9 +17,11 @@ export const Layout = ({ children, title }: Props) => {
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
       </Head>
 
-      {/* <Navbar /> */}
+      <Navbar />
 
-      <main>
+      <main style={{
+        padding: '0 20px',
+      }}>
         {children}
       </main>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,43 @@
-import { Button } from "@nextui-org/react";
-import { Layout } from '../components/layouts';
+import { GetStaticProps } from 'next';
 
-export default function HomePage() {
+import { pokeApi } from '../api';
+import { Layout } from '../components/layouts';
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
+
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+export default function HomePage({ pokemons }: Props) {
   return (
-    <Layout>
-      <Button color="gradient">
-        Hola Mundo
-      </Button>
+    <Layout title='Listado de Pokemons'>
+      <ul>
+        {pokemons.map(({ id, image, name }) => (
+          <li key={id}>
+            <img src={image} alt={name} width={50} />
+            <span style={{ color: 'white' }}>{id} - </span>
+            <span>{name}</span>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const baseUrl = (id: string) => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+  const pokemons = data.results.map((pokemon, index) => ({
+      ...pokemon,
+      id: index + 1,
+      image: baseUrl((index + 1).toString())
+    }
+  ))
+
+  return {
+    props: {
+      pokemons
+    }
+  }
 }
