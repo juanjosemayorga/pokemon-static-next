e0b4663bd1fc3b999229459ec19eb80686496b54
Juan Jose Mayorga <devjuanjose@gmail.com>
2023-01-05 07:32:20 -0600

    add incremental static generation by name and by id

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -110,19 +110,30 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
   const pokemons151 = [...Array(151)].map((_, index) => `${index + 1}`);
 
   return {
-    // paths: [{ params: { id: "1" } }],
     paths: pokemons151.map((id) => ({ params: { id } })),
-    fallback: false
+    fallback: 'blocking',
   }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
 
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id)
+      pokemon
     },
+    revalidate: 86400,
   };
 };
 
